0d46d31dfb064383ce9f7c1c7eb06cc655f3da72
Joel Pereira <joel.pereira.996@gmail.com>
2021-09-23 15:00:42 -0300

    use alert to signal success in creating template

--- options_page/options.js
+++ options_page/options.js
@@ -24,6 +24,34 @@ templateForm.addEventListener('submit', (e) => {
     chrome.storage.sync.set({ templates: toSave }, () => {
       title.value = '';
       content.value = '🔗';
+      displayAlert('Template created.')
     })
   })
 });
+
+
+function displayAlert(message) {
+  const htmlAlert = document.querySelector('.alert');
+
+  if (!htmlAlert.classList.contains('hidden')) return;
+  else {
+    const messageContainer = document.querySelector('.alert-content');
+    messageContainer.innerHTML = '';
+    messageContainer.innerHTML = message;
+
+    htmlAlert.appendChild(messageContainer);
+
+    htmlAlert.classList.remove('hidden');
+
+    const closeButton = document.querySelector('.closebtn');
+
+    const hideAlert = () => {
+      htmlAlert.classList.add('hidden');
+      closeButton.removeEventListener('click', hideAlert);
+    }
+
+    closeButton.addEventListener('click', hideAlert);
+    // remove alert from view in 1.5 seconds
+    setTimeout(() => hideAlert(), 1500);
+  }
+}
